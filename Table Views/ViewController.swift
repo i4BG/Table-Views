dfc3e4e54400f5f1707385814d8f096aa6bf1ba9
Ivaylo Todorov <ivaylo.k.todorov@me.com>
2015-12-07 22:12:17 +0200

    TableViews-iOS
    simple playing with Table Views on iOS

--- Table Views/ViewController.swift
+++ Table Views/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    var cellCount = ["1", "2", "3", "4"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return cellCount.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForAtIndexPath indexPath: NSIndexPath) ->UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = cellCount[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
